26031bcd6165c163080a72596f32d324cf02d8d4
stvanusa <167260653+stvanusa@users.noreply.github.com>
2024-10-01 10:12:14 -0300

    primeiro

---
+++ listaEx01/01/questao01.php
@@ -0,0 +1,5 @@
+<!-- Crie uma variável chamada $nome e atribua a ela seu nome. Em seguida, imprima uma mensagem no formato "Olá, [seu nome]!".-->
+<?php
+$nome = "Vanusa"; 
+echo "Olá, $nome!";
+?>
---
+++ listaEx01/02/questao.php
@@ -0,0 +1,5 @@
+<!-- 2 Declare duas variáveis $a e $b, atribua a elas números inteiros. Calcule e imprima a soma, subtração, multiplicação e divisão. Verificação de Par ou Ímpar -->
+<?php
+$a = 10; 
+$b = 3;
+?>
